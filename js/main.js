703a1a53fde40519a35dd3adeb24294f3b910697
Arnau <arnauaira0@gmail.com>
2024-05-30 13:44:50 +0200

    Penúltima versión

--- js/main.js
+++ js/main.js
@@ -10,3 +10,33 @@ function show_menu_mobile(){
 		body.classList.add('blocked');
 	}
 }
+$(document).ready(function() {
+	$('#num-personas').change(function() {
+		if ($(this).val() === 'more-than-20') {
+			$('#moreThan20Modal').modal('show');
+		}
+	});
+
+	$('#save-personas-btn').click(function() {
+		var specificNum = $('#specific-num-personas').val();
+		if (specificNum && specificNum >= 21) {
+			$('#num-personas').append('<option value="' + specificNum + '">' + specificNum + ' personas</option>');
+			$('#num-personas').val(specificNum);
+			$('#moreThan20Modal').modal('hide');
+		} else {
+			alert('Por favor, introduce un número válido de personas (21 o más).');
+		}
+	});
+});
+/* Función para mostrar/ocultar el menú de hamburguesa */
+function toggleMenu() {
+    var navbar = document.getElementById("navbar");
+    
+    if (navbar.style.display === "block") {
+        navbar.style.display = "none";
+    } else {
+        navbar.style.display = "block";
+    }
+}
+
+
